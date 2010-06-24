38d425e02b29f74ddc74d4ff225ed5c5c32f7601
ben hengst <notbenh@cpan.org>
2010-06-24 10:42:26 -0700

    sigh I am really off with my CPAN-foo

--- lib/Test/Structure.pm
+++ lib/Test/Structure.pm
@@ -14,11 +14,11 @@ Test::Structure - Test for the structure of a package
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
